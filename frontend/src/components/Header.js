458022436db0bc0336f93ae128887bf4f4d75202
SelamT <selamt4709@gmail.com>
2024-05-22 20:19:28 +0300

    Initalizing and structuring the frontend

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+// src/components/Header.js
+
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>Welcome to NLP Data Warehouse</h1>
+    </header>
+  );
+}
+
+export default Header;
